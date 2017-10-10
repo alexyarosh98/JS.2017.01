d6ea05320e5d4a281216a7eef9bdb76098da19d8
Alexander <alexyarosh98@gmail.com>
2017-10-10 23:10:36 +0300

    1.1

--- log.js
+++ log.js
@@ -1,9 +1,9 @@
-for (var i = 0 ; i <data.length; i++) {
+﻿for (var i = 0 ; i <data.length; i++) {
 	console.log("data[%s] = %s", i+1 , checkValue(data[i]));
 }
 
 function checkValue(value) {
-	console.log(value)
+	
 	if (typeof(value)=="number") {
 		return value;
 	}
